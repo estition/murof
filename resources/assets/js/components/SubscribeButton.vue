94c832d1fe4f07ba90d78bccfeace90165905005
estition <raftejeda@gmail.com>
2018-02-04 00:26:57 +0100

    Avoid mutating props while subscribing to a thread resolves #15

--- resources/assets/js/components/SubscribeButton.vue
+++ resources/assets/js/components/SubscribeButton.vue
@@ -5,20 +5,26 @@
 <script>
     export default {
         props: ['active'],
+		
+		data() {
+            return {
+                isActive: this.active
+            }
+        },
 
         computed: {
             classes() {
-                return ['btn', this.active ? 'btn-primary' : 'btn-default'];
+                return ['btn', this.isActive ? 'btn-primary' : 'btn-default'];
             }
         },
 
         methods: {
             subscribe() {
                 axios[
-                    (this.active ? 'delete' : 'post')
+                    (this.isActive ? 'delete' : 'post')
                 ](location.pathname + '/subscriptions');
 
-                this.active = ! this.active;
+                this.isActive = ! this.isActive;
             }
         }
     }
