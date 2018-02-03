98af6c283bad53885d9363fb3472ee71b69d1415
estition <raftejeda@gmail.com>
2018-02-04 00:41:41 +0100

    Call seeders properly resolves #17

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-		$this->call(UsersSeeder::class);
-		$this->call(SampleDataSeeder::class);
+		$this->call([
+            UsersSeeder::class,
+            SampleDataSeeder::class
+        ]);
     }
 }
